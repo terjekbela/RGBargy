52439165faf76337aa42e23c8474d5ab2e452d42
Bela Terjek <bela@netc.hu>
2024-12-08 11:58:49 +0100

    Updating checker example with faster version

--- examples/checker.ino
+++ examples/checker.ino
@@ -1,20 +1,41 @@
 #include <RGBargy.h>
 
-RGBargy rgbg(RGBG_MODE_640x480);
+// Check cpu freq when setting resolution!
+RGBargy rgbg(RGBG_MODE_640x480,     125);
+// RGBargy rgbg(RGBG_MODE_800x600,  120);
+// RGBargy rgbg(RGBG_MODE_1024x768, 175);
 
-void setup() {
-}
+int mw = rgbg.get_mode_width()  / 8;
+int mh = rgbg.get_mode_height() / 8;
+
+void setup() {}
 
 void loop() {
-  int x, y, xd, yd;
-  int m = millis()/1000;
+  int x, y, xd, yd, xs, ys;
+  byte cs;
+  long m = millis()/1000;
   for (yd=0; yd<8; yd++) {
-    for (y=0; y<60; y++) {
+    for (y=0; y<mh; y++) {
+      ys = yd*mh+y;
       for (xd=0; xd<8; xd++) {
-        for (x=0; x<80; x++) {
-          rgbg.pixel(xd*80+x, yd*60+y, (xd+yd+m)%8 );
+        cs = (xd+yd+m)%8;
+        xs = xd*mw;
+        for (x=0; x<mw; x++) {
+          rgbg.pixel(xs+x, ys, cs);
         }
       }
     }
   }
 }
+
+// original simpler but slower version
+// void loop() {
+//   int x  = 0;
+//   int y  = 0;
+//   long m = millis()/1000;
+//   for (y=0; y<mh; y++) {
+//     for (x=0; x<mw; x++) {
+//       rgbg.pixel(x, y, (x / 80 + y / 60 + m) % 8 );
+//     }
+//   }
+// }
